cbe895559f8351fcc68f0d0eb40295382cb469bd
Okan Ülker <okanulkr@hotmail.com>
2022-05-17 19:54:38 +0300

    BL added

--- Controllers/ShipmentController.cs
+++ Controllers/ShipmentController.cs
@@ -9,10 +9,17 @@ namespace FleetManagementApi.Controllers;
 public class ShipmentController : ControllerBase
 {
     private readonly ILogger<ShipmentController> _logger;
+    PackageContext _packageContext;
+    PackageAssignmentContext _packageAssignmentContext;
 
-    public ShipmentController(ILogger<ShipmentController> logger)
+    public ShipmentController(
+        ILogger<ShipmentController> logger,
+        PackageContext packageContext,
+        PackageAssignmentContext packageAssignmentContext)
     {
         _logger = logger;
+        _packageContext = packageContext;
+        _packageAssignmentContext = packageAssignmentContext;
     }
 
     [HttpPost("Ship")]
@@ -21,6 +28,37 @@ public class ShipmentController : ControllerBase
         // TODO
         // update all packages as 'loaded'
         // update related bags as 'loaded'
+
+        foreach (ShipmentRouteRequest point in request.Route!)
+        {
+            foreach (DeliveryItemRequest delivery in point.Deliveries!)
+            {
+                PackageEntity package = _packageContext.Packages.Single(x => x.Barcode == delivery.Barcode);
+                package.State = State.Loaded;
+
+                // package type
+                if (point.DeliveryPoint != 3)
+                {
+                    package.State = State.Unloaded;
+                }
+
+                // bag type
+                if (point.DeliveryPoint != 1)
+                {
+                    List<PackageAssignmentEntity> packageAssignments = _packageAssignmentContext.PackageAssignments
+                        .Where(x => x.BagBarcode == package.Barcode)
+                        .ToList();
+
+                    package.State = State.Unloaded;
+                    foreach (PackageAssignmentEntity packageAssignment in packageAssignments)
+                    {
+                        PackageEntity packageOfBag = _packageContext.Packages.Single(x => x.Barcode == packageAssignment.Barcode);
+                        packageOfBag.State = State.Unloaded;
+                    }
+                }
+            }
+        }
+
         return Ok();
     }
 }
